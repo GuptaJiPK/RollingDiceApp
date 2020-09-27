ea3a192b7e8b26ec3ad0c6606e0f7f091c1844b0
Prateek Gupta <you@example.com>
2020-09-27 15:17:59 +0530

    Complete Image transition without animation

---
+++ android/app/src/main/kotlin/com/example/Rolling_Dice_2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Rolling_Dice_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Homescreen.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class Homescreen extends StatefulWidget {
+  Homescreen({Key key}) : super(key: key);
+
+  @override
+  _HomescreenState createState() => _HomescreenState();
+}
+
+class _HomescreenState extends State<Homescreen> {
+  int leftDice = 1;
+  int rightDice = 1;
+  void roll() {
+    setState(() {
+      leftDice = Random().nextInt(6) + 1;
+      rightDice = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Roll The Dice"),
+      ),
+      // drawer: Drawer(
+      //     child: Container(
+      //       child: Column(
+      //         children: [
+      //           DrawerHeader(
+      //   child: UserAccountsDrawerHeader(
+      //           accountName: Text("User"),
+      //           accountEmail: Text("User@xyz.com"),
+      //   ),
+      // ),
+      //         ],
+      //       ),
+      //     )),
+
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            // DrawerHeader(
+            //   child: Text('Drawer Header'),
+            //   decoration: BoxDecoration(
+            //     color: Colors.blue,
+            //   ),
+            // ),
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                child: Image(
+                  image: AssetImage("assets/images/user.png"),
+                ),
+              ),
+              accountName: Text("User"),
+              accountEmail: Text("user@xyz.com"),
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.home),
+              ),
+              title: Text('Home'),
+              trailing: Icon(Icons.edit),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.map),
+              ),
+              title: Text('Directions'),
+              trailing: Icon(Icons.edit),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              children: [
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image(
+                      image:
+                          AssetImage("assets/images/dice-png-$leftDice.png")),
+                )),
+                Expanded(
+                    child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image(
+                      image:
+                          AssetImage("assets/images/dice-png-$rightDice.png")),
+                ))
+              ],
+            ),
+            RaisedButton(
+              hoverColor: Colors.white,
+              onPressed: roll,
+              child: Text(
+                "Roll",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  //color: Colors.black,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
